631923a3ed33e2bcaf8ec9eac2999500c16df1ce
Elya Megrabyan <m-elia@yandex.ru>
2023-01-30 02:59:50 +0300

    011

---
+++ Ex011_Array2/Program.cs
@@ -0,0 +1,30 @@
+﻿Console.Clear();
+void Fill(int[] collect)
+{
+     int l=collect.Length;
+     int i=0;
+     while (i<l)
+     {
+        collect[i]=new Random().Next(1,10);
+        i++;
+     }
+
+}
+
+
+void Print(int[] col)
+{
+     int count=col.Length;
+     int pos=0;
+     while (pos<count)
+     {
+     Console.WriteLine(col[pos]); 
+     pos++;
+     }
+
+}
+
+int [] array = new int[10];
+
+Fill(array);
+Print(array);
