8174cc45dae2642c9e2db5102f3b39b4a35fc149
Elya Megrabyan <m-elia@yandex.ru>
2023-01-30 02:33:48 +0300

    010

---
+++ Ex010_Method/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+int[] array={1,5,6,7,9,4,3,5,6,8,5,6};
+int n=array.Length;
+int find=6;
+int i=0;
+
+while (i<n)
+{
+if (array[i]==find) {Console.WriteLine(i);
+                     break;}
+
+i++;
+}
+
